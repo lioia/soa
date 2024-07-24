c7cc3e88cc8f263ccb2b38f61ba048d1fb249f42
Alessandro Lioi <lioialessandro2000@live.com>
2024-07-24 10:41:50 +0200

    fix(crypto): comparison and comments

--- reference_monitor/utils/crypto.c
+++ reference_monitor/utils/crypto.c
@@ -1,4 +1,5 @@
 #include "crypto.h"
+#include "../reference_monitor.h"
 
 #include <crypto/hash.h>
 #include <linux/err.h>
@@ -18,39 +19,40 @@ char *crypt_data(const unsigned char *data) {
 
   tfm = crypto_alloc_shash("sha256", 0, 0);
   if (IS_ERR(tfm)) {
-    pr_err("crypto_alloc_shash failed\n");
+    pr_err("%s: crypto_alloc_shash failed\n", MODNAME);
     return NULL;
   }
   desc = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(tfm), GFP_KERNEL);
   if (desc == NULL) {
-    pr_err("kmalloc failed for desc\n");
+    pr_err("%s: kmalloc failed for desc\n", MODNAME);
     goto exit;
   }
   desc->tfm = tfm;
 
   ret = crypto_shash_init(desc);
   if (ret < 0) {
-    pr_err("crypto_shash_init failed\n");
+    pr_err("%s: crypto_shash_init failed\n", MODNAME);
     goto exit;
   }
 
   ret = crypto_shash_update(desc, data, strlen(data));
   if (ret < 0) {
-    pr_err("crypto_shash_update failed\n");
+    pr_err("%s: crypto_shash_update failed\n", MODNAME);
     goto exit;
   }
 
   digest = kmalloc(sizeof(*digest) * 32, GFP_KERNEL);
   if (digest == NULL) {
-    pr_err("kmalloc failed for buffer\n");
+    pr_err("%s: kmalloc failed for buffer\n", MODNAME);
     goto exit;
   }
 
   ret = crypto_shash_final(desc, digest);
   if (ret < 0) {
-    pr_err("crypto_shash_final failed\n");
+    pr_err("%s: crypto_shash_final failed\n", MODNAME);
     goto exit;
   }
+  digest[32 - 1] = '\0';
 
 exit:
   if (desc)
@@ -66,7 +68,7 @@ bool check_hash(const unsigned char *data, const unsigned char *hashed) {
 
   out = crypt_data(data);
   if (out == NULL) {
-    printk(KERN_ERR "failed to crypt data\n");
+    pr_err("%s: crypt_data failed\n", MODNAME);
     return -1;
   }
   return strcmp(out, hashed) == 0;
