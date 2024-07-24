83caaaea8f55867b4093d605a2ac99f60a03b230
Alessandro Lioi <lioialessandro2000@live.com>
2024-07-24 11:52:30 +0200

    refactor(user)

--- reference_monitor/user/user.c
+++ reference_monitor/user/user.c
@@ -1,21 +1,30 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "../reference_monitor.h"
 
-#define CODE_1 174 // depends on the Kernel; TODO: get from module parameters file
+// TODO: get from module parameters file
+#define CHANGE_PASSWORD 156
+#define SET_STATE 174
+#define ADD_PATH 177
+#define DELETE_PATH 178
 
 int main(void) {
   printf("Hello, world\n");
-  long ret = syscall(156, 1, 2);
+  long ret = syscall(CHANGE_PASSWORD, "password", "reference_monitor_default_password");
   if (ret < 0) {
-    printf("syscall 156 failed: %s\n", strerror(errno));
+    printf("syscall %d failed: %s\n", CHANGE_PASSWORD, strerror(errno));
+    exit(EXIT_FAILURE);
   }
-  syscall(CODE_1, "test", REFMON_STATE_ON);
+  puts("Correctly changed password");
+  syscall(SET_STATE, "password", REFMON_STATE_ON);
   if (ret < 0) {
-    printf("syscall %d failed: %s\n", CODE_1, strerror(errno));
+    printf("syscall %d failed: %s\n", SET_STATE, strerror(errno));
+    exit(EXIT_FAILURE);
   }
-  return 0;
+  puts("Correctly activated reference monitor");
+  exit(EXIT_SUCCESS);
 }
