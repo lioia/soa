fb3293a82323632b82f161bd61deb186ed50c8ec
Alessandro Lioi <lioialessandro2000@live.com>
2024-08-28 15:34:50 +0200

    fix(tasks): remove strlen, using saved length to calculate line length

--- reference_monitor/tasks/tasks.c
+++ reference_monitor/tasks/tasks.c
@@ -11,28 +11,24 @@ void write_to_log(unsigned long data) {
   struct reference_monitor_packed_work *work = NULL;
   char *hash = NULL, *line = NULL, *primary_file_path = NULL, *secondary_file_path = NULL;
   struct file *file = NULL;
-  size_t len = 0;
+  size_t len = 0, hash_len = 2 * SHA_LENGTH;
 
   // Get the work
   work = container_of((void *)data, struct reference_monitor_packed_work, the_work);
+  goto exit;
 
   primary_file_path = work->primary_file_path == NULL ? "" : work->primary_file_path;
   secondary_file_path = work->secondary_file_path == NULL ? "" : work->secondary_file_path;
 
-  if (work->operation == NULL)
-    pr_info("%s: operation is null in write_to_log\n", MODNAME);
-  if (primary_file_path == NULL)
-    pr_info("%s: primary_file_path is null in write_to_log\n", MODNAME);
-  if (secondary_file_path == NULL)
-    pr_info("%s: secondary_file_path is null in write_to_log\n", MODNAME);
-  if (work->program_path == NULL)
-    pr_info("%s: program_path is null in write_to_log\n", MODNAME);
+  if (work->secondary_file_path == NULL)
+    pr_info("secondary_file_path is %s\n", secondary_file_path);
 
   // Calculate hash of offending program contents
   hash = crypt_data(work->program_path, true);
   if (hash == NULL) {
     pr_err("%s: crypt_data failed in write_to_log\n", MODNAME);
     hash = "unavailable";
+    hash_len = 10;
   }
 
   // Create output string
@@ -40,13 +36,13 @@ void write_to_log(unsigned long data) {
   // Calculating overfitted buffer for line
   // - 4 * 10: max number of digits for the ids
   // - 9: commas, newline, NULL-terminator
-  len = strlen(work->operation) + 4 * 10 + strlen(primary_file_path) + strlen(secondary_file_path) +
-        strlen(work->program_path) + strlen(hash) + 9;
+  len = work->operation_len + 4 * 10 + work->primary_file_path_len + work->secondary_file_path_len +
+        work->program_path_len + hash_len + 9;
 
-  line = kmalloc(sizeof(*line) * len, GFP_ATOMIC);
+  line = kzalloc(sizeof(*line) * len, GFP_KERNEL);
   if (line == NULL) {
     pr_err("%s: kmalloc for line failed in write_to_log\n", MODNAME);
-    pr_info("%s-log: %s,%d,%d,%d,%d,%s,%s,%s,%s", MODNAME, work->operation, work->tgid, work->tid, work->uid,
+    pr_info("%s-log: %s,%d,%d,%d,%d,%s,%s,%s,%s\n", MODNAME, work->operation, work->tgid, work->tid, work->uid,
             work->euid, primary_file_path, secondary_file_path, work->program_path, hash);
     goto exit;
   }
@@ -77,7 +73,7 @@ exit:
     kfree(work->primary_file_path);
   if (work->secondary_file_path)
     kfree(work->secondary_file_path);
-  if (hash)
+  if (hash && hash_len != 10)
     kfree(hash);
   if (file && !IS_ERR(file))
     filp_close(file, NULL);
--- reference_monitor/tasks/tasks.h
+++ reference_monitor/tasks/tasks.h
@@ -4,15 +4,19 @@
 #include <linux/workqueue.h>
 
 struct reference_monitor_packed_work {
-  pid_t tgid;                  // Program TGID
-  pid_t tid;                   // Thread ID
-  uid_t uid;                   // User ID
-  uid_t euid;                  // Effective User ID
-  char *program_path;          // Offending Program Path
-  char *primary_file_path;     // Offending File Path (primary)
-  char *secondary_file_path;   // Offending File Path (secondary)
-  char *operation;             // Operation
-  struct work_struct the_work; // `data`
+  pid_t tgid;                     // Program TGID
+  pid_t tid;                      // Thread ID
+  uid_t uid;                      // User ID
+  uid_t euid;                     // Effective User ID
+  char *program_path;             // Offending Program Path
+  size_t program_path_len;        // Program Path Length
+  char *primary_file_path;        // Offending File Path (primary)
+  size_t primary_file_path_len;   // Primary File Path Length
+  char *secondary_file_path;      // Offending File Path (secondary)
+  size_t secondary_file_path_len; // Secondary File Path Length
+  char *operation;                // Operation
+  size_t operation_len;           // Operation Length
+  struct work_struct the_work;    // `data`
 };
 
 void write_to_log(unsigned long data);
